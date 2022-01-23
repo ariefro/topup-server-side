a6ac450d5f3e2cd4d1877441cc35a239e8ef3e4a
Arief romadhon <ariefromadhon26@gmail.com>
2022-01-24 06:45:21 +0700

    add payment model

---
+++ src/models/payment.js
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+
+const paymentSchema = Schema({
+  type: {
+    type: String,
+    require: [true, 'tipe pembayaran harus diisi'],
+  },
+  status: {
+    type: String,
+    enum: ['active', 'non active'],
+    default: 'active',
+  },
+  banks: {
+    type: Schema.Types.ObjectId,
+    ref: 'Bank',
+  },
+});
+
+const Payment = model('Payment', paymentSchema);
+
+export default Payment;
