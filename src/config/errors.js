8a8bbd5252fcc62412d997083c115cb7ee4e7998
Arief romadhon <ariefromadhon26@gmail.com>
2022-01-31 20:35:32 +0700

    add base controller for  handle error

--- src/config/errors.js
+++ src/config/errors.js
@@ -1,6 +1,8 @@
 const ERRORS = {
   INCOMPLETE_INPUT: 'incomplete_input',
   NOT_FOUND: 'not_found',
+  BAD_REQUEST: 'bad request',
+  EMAIL_OR_PASSWORD_DOESNT_MATCH: 'email_or_password_doesnt_match',
 };
 
 export default ERRORS;
---
+++ src/controllers/base-controller.js
@@ -0,0 +1,30 @@
+import ERRORS from '../config/errors';
+
+class BaseController {
+  static getError = (err) => {
+    switch (err.message) {
+      case ERRORS.INCOMPLETE_INPUT: return {
+        code: 400,
+        message: 'Incomplete input',
+      };
+      case ERRORS.NOT_FOUND: return {
+        code: 404,
+        message: 'Not found',
+      };
+      case ERRORS.BAD_REQUEST: return {
+        code: 400,
+        message: 'Bad request',
+      };
+      case ERRORS.EMAIL_OR_PASSWORD_DOESNT_MATCH: return {
+        code: 400,
+        message: 'Email or Password does not match',
+      };
+      default: return {
+        code: 500,
+        message: 'Internal server error',
+      };
+    }
+  };
+}
+
+export default BaseController;
